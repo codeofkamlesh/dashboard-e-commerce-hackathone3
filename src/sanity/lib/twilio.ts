c9ab6fabbce134e7eba9594dc0593780ac4d9c27
KAMLESH KUMAR <kamlesh_kumar174@yahoo.com>
2025-02-06 22:30:11 +0500

    updated

--- src/sanity/lib/twilio.ts
+++ src/sanity/lib/twilio.ts
@@ -1,17 +1,17 @@
-import twilio from "twilio";
+// import twilio from "twilio";
 
-const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+// const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
-export const sendOTP = async (phoneNumber: string) => {
-  const verification = await client.verify.v2
-    .services(process.env.TWILIO_VERIFY_SERVICE_SID)
-    .verifications.create({ to: phoneNumber, channel: "sms" });
-  return verification;
-};
+// export const sendOTP = async (phoneNumber: string) => {
+//   const verification = await client.verify.v2
+//     .services(process.env.TWILIO_VERIFY_SERVICE_SID)
+//     .verifications.create({ to: phoneNumber, channel: "sms" });
+//   return verification;
+// };
 
-export const verifyOTP = async (phoneNumber: string, code: string) => {
-  const verificationCheck = await client.verify.v2
-    .services(process.env.TWILIO_VERIFY_SERVICE_SID)
-    .verificationChecks.create({ to: phoneNumber, code });
-  return verificationCheck;
-};
+// export const verifyOTP = async (phoneNumber: string, code: string) => {
+//   const verificationCheck = await client.verify.v2
+//     .services(process.env.TWILIO_VERIFY_SERVICE_SID)
+//     .verificationChecks.create({ to: phoneNumber, code });
+//   return verificationCheck;
+// };
